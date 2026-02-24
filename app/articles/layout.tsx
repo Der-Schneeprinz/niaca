99cbf046da4bd199f25f325c748facf498002b83
Der Kreidekönig <mcolaguer@iskolarngbayan.pup.edu.ph>
2026-02-24 10:21:58 +0800

    text formatting resolved

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import "./style.css"
 
 interface MDXPage {
     children: React.ReactNode
 }
 
+
+
 function layout({ children }: MDXPage) {
     return (
-        <div className='antialiased max-w-xl mx-4 mt-8 lg:mx-auto geist'>{children}</div>
+        <div className='antialiased max-w-xl mx-4 lg:mx-auto geist flex flex-col format'>{children}</div>
     )
 }
 
